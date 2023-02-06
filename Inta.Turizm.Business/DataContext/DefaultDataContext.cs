72a58f4720a6b578e48092b6e5860189df254d68
Erdoğan Kaba <erdogankb57@gmail.com>
2023-02-06 21:25:10 +0300

    .

---
+++ Inta.Turizm.Business/DataContext/DefaultDataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Inta.Turizm.Business.DataContext
+{
+    public class DefaultDataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DefaultDataContext()
+        {
+            _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultDataContext"));
+            optionsBuilder.EnableSensitiveDataLogging(true);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("dbo");
+        }
+
+        //public DbSet<GeneralSettings> GeneralSettingss { get; set; }
+
+
+    }
+
+}
