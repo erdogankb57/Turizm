4ed5fe6ba1ec94cecca6571f2788d031fc9b5be3
Erdoğan Kaba <erdogankb57@gmail.com>
2023-02-07 23:34:38 +0300

    .

--- Inta.Turizm.Business/Mapping/MappingProfile.cs
+++ Inta.Turizm.Business/Mapping/MappingProfile.cs
@@ -10,6 +10,108 @@ namespace Inta.Turizm.Business.Mapping
     {
         public MappingProfile()
         {
+                   CreateMap<HotelDto, Hotel>()
+                  .ForMember(dest => dest.Id, src => src.MapFrom(a => a.Id))
+                  .ForMember(dest => dest.Name, src => src.MapFrom(a => a.Name))
+                  .ForMember(dest => dest.Explanation, src => src.MapFrom(a => a.Explanation))
+                  .ForMember(dest => dest.Adress, src => src.MapFrom(a => a.Adress))
+                  .ForMember(dest => dest.Phone, src => src.MapFrom(a => a.Phone))
+                  .ForMember(dest => dest.Logo, src => src.MapFrom(a => a.Logo))
+                  .ForMember(dest => dest.RecordDate, src => src.MapFrom(a => a.RecordDate))
+                  .ForMember(dest => dest.IsActive, src => src.MapFrom(a => a.IsActive))
+                  .ForMember(dest => dest.HotelImages, src => src.MapFrom(a => a.HotelImages.Select(n => new HotelImage
+                  {
+                      Image = n.Image,
+                      RecordDate = n.RecordDate,
+                      Name = n.Name,
+                      IsActive = n.IsActive,
+                      HotelId = n.HotelId,
+                      Id = n.Id
+                  })))
+                  .ForMember(dest => dest.HotelRooms, src => src.MapFrom(a => a.HotelRooms.Select(n => new HotelRoom
+                  {
+                      Explanation = n.Explanation,
+                      HotelId = n.HotelId,
+                      Id = n.Id,
+                      IsActive = n.IsActive,
+                      Name = n.Name,
+                      NumberOfPeople = n.NumberOfPeople,
+                      RecordDate = n.RecordDate,
+                      HotelRoomImages = n.HotelRoomImages.Select(s => new HotelRoomImage
+                      {
+                          RecordDate = s.RecordDate,
+                          HotelRoomlId = s.HotelRoomlId,
+                          Image = s.Image,
+                          Id = s.Id,
+                          IsActive = s.IsActive,
+                          Name = s.Name
+                      }).ToList()
+                  })));
+
+                  CreateMap<Hotel, HotelDto>()
+                  .ForMember(dest => dest.Id, src => src.MapFrom(a => a.Id))
+                  .ForMember(dest => dest.Name, src => src.MapFrom(a => a.Name))
+                  .ForMember(dest => dest.Explanation, src => src.MapFrom(a => a.Explanation))
+                  .ForMember(dest => dest.Adress, src => src.MapFrom(a => a.Adress))
+                  .ForMember(dest => dest.Phone, src => src.MapFrom(a => a.Phone))
+                  .ForMember(dest => dest.Logo, src => src.MapFrom(a => a.Logo))
+                  .ForMember(dest => dest.RecordDate, src => src.MapFrom(a => a.RecordDate))
+                  .ForMember(dest => dest.IsActive, src => src.MapFrom(a => a.IsActive))
+                  .ForMember(dest => dest.HotelImages, src => src.MapFrom(a => a.HotelImages.Select(n => new HotelImageDto
+                  {
+                      Image = n.Image,
+                      RecordDate = n.RecordDate,
+                      Name = n.Name,
+                      IsActive = n.IsActive,
+                      HotelId = n.HotelId,
+                      Id = n.Id
+                  })))
+                  .ForMember(dest => dest.HotelRooms, src => src.MapFrom(a => a.HotelRooms.Select(n => new HotelRoomDto
+                  {
+                      Explanation = n.Explanation,
+                      HotelId = n.HotelId,
+                      Id = n.Id,
+                      IsActive = n.IsActive,
+                      Name = n.Name,
+                      NumberOfPeople = n.NumberOfPeople,
+                      RecordDate = n.RecordDate,
+                      HotelRoomImages = n.HotelRoomImages.Select(s => new HotelRoomImageDto
+                      {
+                          RecordDate = s.RecordDate,
+                          HotelRoomlId = s.HotelRoomlId,
+                          Image = s.Image,
+                          Id = s.Id,
+                          IsActive = s.IsActive,
+                          Name = s.Name
+                      }).ToList()
+                  })));
+
+
+
+            CreateMap<DataResult<Hotel>, DataResult<HotelDto>>()
+               .ForMember(dest => dest.Data, src => src.MapFrom(s => new HotelDto
+               {
+                   Adress = s.Data.Adress,
+                   Explanation = s.Data.Explanation,
+                   Id = s.Data.Id,
+                   IsActive = s.Data.IsActive,
+                   Logo = s.Data.Phone,
+                   Name = s.Data.Name,
+                   Phone = s.Data.Phone,
+                   RecordDate = s.Data.RecordDate,
+                   HotelImages = s.Data.HotelImages.Select(n => new HotelImageDto
+                   {
+                       Image = n.Image,
+                       RecordDate = n.RecordDate,
+                       Name = n.Name,
+                       IsActive = n.IsActive,
+                       HotelId = n.HotelId,
+                       Id = n.Id
+                   }).ToList()
+               }));
+
+
+
             CreateMap<DataResult<List<Hotel>>, DataResult<List<HotelDto>>>()
                 .ForMember(dest =>
                 dest.Data,
@@ -56,66 +158,9 @@ namespace Inta.Turizm.Business.Mapping
                     )
                ));
 
-            CreateMap<DataResult<Hotel>, DataResult<HotelDto>>()
-                .ForMember(dest => dest.Data, src => src.MapFrom(s => new HotelDto
-                {
-                    Adress = s.Data.Adress,
-                    Explanation = s.Data.Explanation,
-                    Id = s.Data.Id,
-                    IsActive = s.Data.IsActive,
-                    Logo = s.Data.Phone,
-                    Name = s.Data.Name,
-                    Phone = s.Data.Phone,
-                    RecordDate = s.Data.RecordDate,
-                    HotelImages = s.Data.HotelImages.Select(n => new HotelImageDto
-                    {
-                        Image = n.Image,
-                        RecordDate = n.RecordDate,
-                        Name = n.Name,
-                        IsActive = n.IsActive,
-                        HotelId = n.HotelId,
-                        Id = n.Id
-                    }).ToList()
-                }));
+           
 
-
-            CreateMap<HotelDto, Hotel>()
-                  .ForMember(dest => dest.Id, src => src.MapFrom(a => a.Id))
-                  .ForMember(dest => dest.Name, src => src.MapFrom(a => a.Name))
-                  .ForMember(dest => dest.Explanation, src => src.MapFrom(a => a.Explanation))
-                  .ForMember(dest => dest.Adress, src => src.MapFrom(a => a.Adress))
-                  .ForMember(dest => dest.Phone, src => src.MapFrom(a => a.Phone))
-                  .ForMember(dest => dest.Logo, src => src.MapFrom(a => a.Logo))
-                  .ForMember(dest => dest.RecordDate, src => src.MapFrom(a => a.RecordDate))
-                  .ForMember(dest => dest.IsActive, src => src.MapFrom(a => a.IsActive))
-                  .ForMember(dest => dest.HotelImages, src => src.MapFrom(a => a.HotelImages.Select(n => new HotelImage
-                  {
-                      Image = n.Image,
-                      RecordDate = n.RecordDate,
-                      Name = n.Name,
-                      IsActive = n.IsActive,
-                      HotelId = n.HotelId,
-                      Id = n.Id
-                  })))
-                  .ForMember(dest => dest.HotelRooms, src => src.MapFrom(a => a.HotelRooms.Select(n => new HotelRoom
-                  {
-                      Explanation = n.Explanation,
-                      HotelId = n.HotelId,
-                      Id = n.Id,
-                      IsActive = n.IsActive,
-                      Name = n.Name,
-                      NumberOfPeople = n.NumberOfPeople,
-                      RecordDate = n.RecordDate,
-                      HotelRoomImages = n.HotelRoomImages.Select(s => new HotelRoomImage
-                      {
-                          RecordDate = s.RecordDate,
-                          HotelRoomlId = s.HotelRoomlId,
-                          Image = s.Image,
-                          Id = s.Id,
-                          IsActive = s.IsActive,
-                          Name = s.Name
-                      }).ToList()
-                  })));
+            
 
 
 
