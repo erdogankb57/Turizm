ee1ae65bb1a04f75c5f6588c47ef6d5e14b14618
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2023-09-01 23:48:31 +0300

    .

--- Inta.Turizm.Api/Controllers/HotelController.cs
+++ Inta.Turizm.Api/Controllers/HotelController.cs
@@ -3,7 +3,6 @@ using Inta.Turizm.Core.Model;
 using Inta.Turizm.Dto.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq.Expressions;
 
 namespace Inta.Turizm.Api.Controllers
 {
@@ -22,7 +21,9 @@ namespace Inta.Turizm.Api.Controllers
         [HttpGet("GetHotelList")]
         public DataResult<List<HotelDto>> Get()
         {
-            return _hotelService.Find(v => v.Id>0, new string[] { "HotelImages", "HotelRooms", "HotelRooms.HotelRoomImages" }, 0, 10);
+            var data = _hotelService.Find(v => v.Id>0, new string[] { "HotelImages", "HotelRooms", "HotelRooms.HotelRoomImages" }, 0, 10);
+            
+            return data;
         }
 
         [HttpGet("{id}")]
