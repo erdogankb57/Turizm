026dcade6803f2980bebe4ed8925b102855e6acd
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2023-09-21 22:04:10 +0300

    Delete UdemyConsoleDocker directory

--- UdemyConsoleDocker/UdemyConsoleDocker/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-int i = 0;
-while (i<1000)
-{
-    Console.WriteLine(i.ToString());
-
-    System.Threading.Thread.Sleep(1000);
-}
